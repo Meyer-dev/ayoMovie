2ab1865c8d30ec542a4d28ef9b5b3a46d7ae209e
Renier <meyerweitz1@gmail.com>
2022-08-31 16:08:13 +0300

    search feature module and component

---
+++ src/app/modules/search/search.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SearchComponent } from './search.component';
+import { SearchRoutingModule } from './search-routing.module';
+//TODO:see if all above modules are needed.
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SearchRoutingModule
+    //TODO:see if feature modules should have all these modules, check layouts module. place in shared modeule?
+  ],
+  declarations: [
+    SearchComponent
+  ]
+})
+export class SearchModule {
+}
