872c29d5ec9398622f4d30cc2583c1120a9c6799
Renier <meyerweitz1@gmail.com>
2022-08-31 16:05:35 +0300

    udate to routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonLayoutComponent } from './layouts/common-layout/common-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CommonLayoutComponent,
+      children: [
+      {  path: '', redirectTo: 'search', pathMatch: 'full'},
+      {
+        path: 'search',
+        loadChildren: () =>
+          import('./modules/search/search.module').then((mod) => mod.SearchModule),
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/search/search-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { MainLayout } from 'src/app/layouts/layouts.service';
+import { SearchComponent } from './search.component';
+
+ const routes: Routes = [
+    MainLayout.childRoutes([
+     { path: '', redirectTo: 'search', pathMatch: 'full' },
+     { path: 'search', component: SearchComponent},
+    ])
+    //TODO:lazy load components.
+ ];
+
+ @NgModule({
+   imports: [RouterModule.forChild(routes)],
+   exports: [RouterModule],
+   providers: []
+ })
+ export class SearchRoutingModule { }
+
