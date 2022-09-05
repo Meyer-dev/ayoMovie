94f074a1f544b77df4e1a57e4301d0ca2a46fe65
Renier <meyerweitz1@gmail.com>
2022-09-05 15:57:31 +0300

    API explanation for movie service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -7,20 +7,29 @@ import { IMoviePreference } from '../shared/interfaces/IMoviePreference';
 @Injectable({providedIn: 'root'})
 export class MovieService {
 
+  // The below value is the API key required to make requests to the OMDb API @ https://omdbapi.com/
+  public apiKey: string = "f590ae4d";
+
   constructor(
     private http: HttpClient,
   ) {}
 
   searchMovie(data: IMoviePreference): Observable<any> {
     const title = data.title;
+    // type value options are "movie" or "series". "movie" has been used here for the movie search app.
     const type = 'movie';
+    // The year values can be any year. No year is passed from the search form as searching by year is not common practice.
     const year = data.year;
+    // plot_format value options are "full" or "short". "full" has been implemented here to access the full plot description.
     const plot_format = 'full';
     //TODO: place url credentials somewhere else in shared ...? dont place const for all, use data.
-    const apiURL = `http://www.omdbapi.com/?apikey=f590ae4d&t=${title}&y=${year}&type=${type}&plot=${plot_format}`;
+    const apiURL = `http://www.omdbapi.com/?apikey=${this.apiKey}&t=${title}&y=${year}&type=${type}&plot=${plot_format}`;
+    // The below data mapping could be further abstracted for a more general case
+    // (i.e more movies returned by API) if the returned data structure was known.
+    // If the returned response value "False" the number of movies is set to 0 (i.e no movies).
     return this.http
       .get(apiURL)
-      .pipe(//TODO abstrat below for more general case
+      .pipe( 
         map((body: any) => 
             {if (body.Response === "True"){
                 const movies = []
