5ee66ba1f66a7abdb836a8eceec04f217911395f
Renier <meyerweitz1@gmail.com>
2022-09-05 17:53:50 +0300

    test update

--- src/app/modules/search/search.component.spec.ts
+++ src/app/modules/search/search.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
@@ -31,6 +30,5 @@ describe('SearchComponent', () => {
       .withContext('after search initialised')
       .toBe(true);
   });
-
 
 });
